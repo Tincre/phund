bdf16f56890725ac9452d0bc3657987cf18a0c31
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-01-29 14:37:17 -0600

    :sparkles: add initial index stock page [sc-533]

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+function Index() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <p>
+        Welcome to your <span className="font-bold italic"> funded </span>{" "}
+        instance!
+      </p>
+    </div>
+  );
+}
+
+export default Index;
