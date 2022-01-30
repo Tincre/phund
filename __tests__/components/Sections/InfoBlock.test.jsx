26d27ac95be87574edbbba31ddfecc897c268677
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-01-30 08:46:41 -0600

    :recycle: :white_check_mark: update naming for InfoBlock change

---
+++ __tests__/components/Sections/InfoBlock.test.jsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import InfoBlock from "../../../components/Sections/InfoBlock";
+
+describe("InfoBlock ", () => {
+  it("should render the component", () => {
+    render(<InfoBlock />);
+  });
+});
--- __tests__/components/Sections/InfoCarousel.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import InfoCarousel from "../../../components/Sections/InfoCarousel";
-
-describe("InfoCarousel", () => {
-  it("should render the component", () => {
-    render(<InfoCarousel />);
-  });
-});
