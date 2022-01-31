ed92a444f45c5f1adce87ddcde5435072615160d
Philip Muellerschoen <phil@musicfox.io>
2022-01-31 16:11:58 -0500

    Update cms.data.js
    ✏️  to copy. snarky

--- cms.data.js
+++ cms.data.js
@@ -18,62 +18,62 @@ export const stats1Data = [
   },
   {
     icon : "",
-    numberStat : "30,000,000",
-    text : "A giant achievement",
+    numberStat : "$1,000,000",
+    text : "A big sales number",
   },
   {
     icon : "",
-    numberStat : "30,000,000",
-    text : "A giant achievement",
+    numberStat : "150%",
+    text : "An impressive growth benchmark ",
   },
   {
     icon : "",
-    numberStat : "30,000,000",
-    text : "A giant achievement",
+    numberStat : "15,000",
+    text : "A testament to your firm's popularity",
   },
 ];
 export const whyInvestContent = {
   subTitle : "Scaling our formula",
   title : "We're seeking investment to release more kick ass brands",
   description :
-      "Our formula for success works and we are scaling it by releasing brands in financial services, non-profit fundraising, and fashion.",
-  cta : "Learn about our SAFE note",
+      "Our formula for success works and we are scaling it by releasing stuff that customers love.",
+  cta : "Learn about how to invest",
   whyInvestCardData : [
-    "The most epic management team in the business of the web.",
-    "Can't stop won't stop.",
-    "Hyper-efficient semi-autonomous subsidiaries crafted for corporate accretive acquisitions.",
+    "The most epic management team in the world.",
+    "With a proven track record of scaling productsand teams.",
+    "Makes this journey a piece of cake.",
   ],
 };
 export const factCardsContent = [
   {
-    title : "This is a bold, awesome fact",
+    title : "We make customers happy",
     description :
-        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+        "Our solution makes a lot of people happy. Don't trust us, Google it.",
   },
   {
-    title : "This is a bold, awesome fact",
+    title : "We are visionaries",
     description :
-        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+        "Our product and process is superior to anything on the market. Try it.",
   },
   {
-    title : "This is a bold, awesome fact",
+    title : "Disrupting is what we do",
     description :
-        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+        "Our technology levels the playing field and will east the big guy's lunch.",
   },
   {
-    title : "This is a bold, awesome fact",
+    title : "The sky is the limit.",
     description :
-        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+        "Every beginning is difficult but we've validated our position time and again.",
   },
   {
-    title : "This is a bold, awesome fact",
+    title : "We are primed for growth",
     description :
-        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+        "The time to put the pedal to the floor is now. We like to drive fast.",
   },
   {
-    title : "This is a bold, awesome fact",
+    title : "Our team is the best",
     description :
-        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+        "We've combined the best possible talent to get the job done.",
   },
 ];
 export const factSnippetsCardContent = [
@@ -147,57 +147,57 @@ export const teamCardContent = [
 ];
 export const fAQCardContent = [
   {
-    title : "How do you accomplish so much?",
+    title : "How much are we raising?",
     listItems : [
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "We are looking to raise a lot", href : "https://tincre.com"},
+      {text : "And made it easy for you to give us money", href : "https://tincre.com"},
+      {text : "The terms are straightforward, href : "https://tincre.com"},
+      {text : "And there's a special kicker for you", href : "https://tincre.com"},
     ],
   },
   {
-    title : "How do you accomplish so much?",
+    title : "How will we use the funds?",
     listItems : [
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Our office needs vending machines", href : "https://tincre.com"},
+      {text : "To make all of our new employees happy", href : "https://tincre.com"},
+      {text : "There also needs to be a slush fund", href : "https://tincre.com"},
+      {text : "To market and sell out our office parties", href : "https://tincre.com"},
     ],
   },
   {
-    title : "How do you accomplish so much?",
+    title : "Why are we the right team?",
     listItems : [
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Lucy went to university and studied hard", href : "https://tincre.com"},
+      {text : "Johnny built a business and sold it fast", href : "https://tincre.com"},
+      {text : "Lauren learned how to code and build an app, href : "https://tincre.com"},
+      {text : "Thomas has people skills and interned at Initech", href : "https://tincre.com"},
     ],
   },
   {
-    title : "How do you accomplish so much?",
+    title : "What is our plan for the future? ",
     listItems : [
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "No crystal ball needed", href : "https://tincre.com"},
+      {text : "We got a solid plan to put your money to work, href : "https://tincre.com"},
+      {text : "Our people will execute with lazer focus , href : "https://tincre.com"},
+      {text : "To grow the company, href : "https://tincre.com"},
     ],
   },
   {
-    title : "How do you accomplish so much?",
+    title : "What do investors get?",
     listItems : [
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "A nice pat on the back", href : "https://tincre.com"},
+      {text : "Followed by five high fives", href : "https://tincre.com"},
+      {text : "A quarterly report directly to their Kindle, href : "https://tincre.com"},
+      {text : "And a seat at the big kids table", href : "https://tincre.com"},
     ],
   },
   {
-    title : "How do you accomplish so much?",
+    title : "What's our exit strategy?",
     listItems : [
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
-      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Build and go public", href : "https://tincre.com"},
+      {text : "Sell to the highest bidder, href : "https://tincre.com"},
+      {text : "Go big or go home", href : "https://tincre.com"},
+      {text : "Quitting is not one of the answers", href : "https://tincre.com"},
     ],
   },
 ];
