dc71ad5cdb79616a608c77d476d8517efb114b15
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-01-31 17:22:23 -0600

    :sparkles: :wrench: add default seo [sc-585]

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,12 @@
 import "../styles/global.css";
 import { SessionProvider } from "next-auth/react";
+import { DefaultSeo } from "next-seo";
+import { defaultSeoContent } from "../siteMetadata";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <DefaultSeo {...defaultSeoContent} />
       <Component {...pageProps} />
     </SessionProvider>
   );
