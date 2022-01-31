6e0785c4ba70c4f794da9a526a77a1185586c066
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-01-31 10:03:28 -0600

    :sparkles: add initial cms data file [sc-589]

---
+++ cms.data.js
@@ -0,0 +1,291 @@
+export const entityTitle = "Tincre";
+export const title = `Funded, by ${entityTitle}`;
+export const description =
+    "A Single Page Application for getting funded, built on Next.js and Tailwindcss, by Tincre.";
+export const author = "Tincre Engineering, https://tincre.com";
+export const navigationLinks =
+    [ `Investing in ${entityTitle}`, "About", "The Team" ];
+export const navigationHrefs =
+    [ "#why-invest-part-1", "#fact-snippets", "#team" ];
+export const cta = "Invest";
+export const logoSrc =
+    "https://res.cloudinary.com/tincre/image/upload/v1638892843/tincre.com/tincre-brand-indigo-800_uatfej.svg";
+export const stats1Data = [
+  {
+    icon : "",
+    numberStat : "30,000,000",
+    text : "A giant achievement",
+  },
+  {
+    icon : "",
+    numberStat : "30,000,000",
+    text : "A giant achievement",
+  },
+  {
+    icon : "",
+    numberStat : "30,000,000",
+    text : "A giant achievement",
+  },
+  {
+    icon : "",
+    numberStat : "30,000,000",
+    text : "A giant achievement",
+  },
+];
+export const whyInvestContent = {
+  subTitle : "Scaling our formula",
+  title : "We're seeking investment to release more kick ass brands",
+  description :
+      "Our formula for success works and we are scaling it by releasing brands in financial services, non-profit fundraising, and fashion.",
+  cta : "Learn about our SAFE note",
+  whyInvestCardData : [
+    "The most epic management team in the business of the web.",
+    "Can't stop won't stop.",
+    "Hyper-efficient semi-autonomous subsidiaries crafted for corporate accretive acquisitions.",
+  ],
+};
+export const factCardsContent = [
+  {
+    title : "This is a bold, awesome fact",
+    description :
+        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+  },
+  {
+    title : "This is a bold, awesome fact",
+    description :
+        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+  },
+  {
+    title : "This is a bold, awesome fact",
+    description :
+        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+  },
+  {
+    title : "This is a bold, awesome fact",
+    description :
+        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+  },
+  {
+    title : "This is a bold, awesome fact",
+    description :
+        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+  },
+  {
+    title : "This is a bold, awesome fact",
+    description :
+        "It's like the world is stuck in a car wash and Tincre is the car. Life is now complete.",
+  },
+];
+export const factSnippetsCardContent = [
+  {
+    description :
+        "A nice, detailed explanation of something with-respect-to this funding rounding.",
+    imageSrc : "/images/indigo-500-avatar.png",
+    title : "A Product KPI",
+    subTitle : "Performance category",
+  },
+  {
+    description :
+        "A nice, detailed explanation of something with-respect-to this funding rounding.",
+    imageSrc : "/images/indigo-500-avatar.png",
+    title : "A Product KPI",
+    subTitle : "Performance category",
+  },
+  {
+    description :
+        "A nice, detailed explanation of something with-respect-to this funding rounding.",
+    imageSrc : "/images/indigo-500-avatar.png",
+    title : "A Product KPI",
+    subTitle : "Performance category",
+  },
+  {
+    description :
+        "A nice, detailed explanation of something with-respect-to this funding rounding.",
+    imageSrc : "/images/indigo-500-avatar.png",
+    title : "A Product KPI",
+    subTitle : "Performance category",
+  },
+  {
+    description :
+        "A nice, detailed explanation of something with-respect-to this funding rounding.",
+    imageSrc : "/images/indigo-500-avatar.png",
+    title : "A Product KPI",
+    subTitle : "Performance category",
+  },
+  {
+    description :
+        "A nice, detailed explanation of something with-respect-to this funding rounding.",
+    imageSrc : "/images/indigo-500-avatar.png",
+    title : "A Product KPI",
+    subTitle : "Performance category",
+  },
+];
+export const teamCardContent = [
+  {
+    name : "Philip Muellerschoen",
+    position : "CEO, Co-Founder",
+    description :
+        "Phil guides the firm in all aspects both internally and externally, operating and growing its brands.",
+    imageSrc : "/team/phil.webp",
+    imageAlt : "Philip Muellerschoen",
+  },
+  {
+    name : "Jason R. Stevens, CFA",
+    position : "CTO+CFO, Co-Founder",
+    description :
+        "Anti-script kiddie crushing the keys to build an entirely new world in which he owns all human-associated items.",
+    imageSrc : "/team/jason.webp",
+    imageAlt : "Jason R. Stevens, CFA",
+  },
+  {
+    name : "Dan Fratoni",
+    position : "Partner, External Brand Relationships",
+    description : "Drives entertainment industry representation for the firm.",
+    imageSrc : "/team/dan.webp",
+    imageAlt : "Dan Fratoni",
+  },
+];
+export const fAQCardContent = [
+  {
+    title : "How do you accomplish so much?",
+    listItems : [
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+    ],
+  },
+  {
+    title : "How do you accomplish so much?",
+    listItems : [
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+    ],
+  },
+  {
+    title : "How do you accomplish so much?",
+    listItems : [
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+    ],
+  },
+  {
+    title : "How do you accomplish so much?",
+    listItems : [
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+    ],
+  },
+  {
+    title : "How do you accomplish so much?",
+    listItems : [
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+    ],
+  },
+  {
+    title : "How do you accomplish so much?",
+    listItems : [
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+      {text : "Work hard, play hard", href : "https://tincre.com"},
+    ],
+  },
+];
+export const footerItems = [
+  {
+    title : "Company",
+    footerColumnItems : [
+      {
+        text : `About ${entityTitle}`,
+        href : "https://tincre.com/",
+      },
+      {
+        text : `Team`,
+        href : "https://tincre.com/about",
+      },
+      {
+        text : "Brands",
+        href : "https://tincre.com/#our-brands",
+      },
+      {
+        text : "Blog",
+        href : "https://slightlysharpe.com",
+      },
+    ],
+  },
+  {
+    title : "Legal",
+    footerColumnItems : [
+      {
+        text : `About ${entityTitle}`,
+        href : "https://tincre.com/",
+      },
+      {
+        text : `Team`,
+        href : "https://tincre.com/about",
+      },
+      {
+        text : "Brands",
+        href : "https://tincre.com/#our-brands",
+      },
+    ],
+  },
+  {
+    title : "Product Categories",
+    footerColumnItems : [
+      {
+        text : `About ${entityTitle}`,
+        href : "https://tincre.com/",
+      },
+      {
+        text : `Team`,
+        href : "https://tincre.com/about",
+      },
+      {
+        text : "Brands",
+        href : "https://tincre.com/#our-brands",
+      },
+    ],
+  },
+  {
+    title : "Development",
+    footerColumnItems : [
+      {
+        text : `About ${entityTitle}`,
+        href : "https://tincre.com/",
+      },
+      {
+        text : `Team`,
+        href : "https://tincre.com/about",
+      },
+      {
+        text : "Brands",
+        href : "https://tincre.com/#our-brands",
+      },
+    ],
+  },
+];
+export const socials = [
+  {
+    href : "https://twitter.com/tincre",
+    displayText : "Twitter",
+  },
+  {
+    href : "https://linkedin.com/company/tincre",
+    displayText : "LinkedIn",
+  },
+  {
+    href : "https://instagram.com/webslastmile",
+    displayText : "Instagram",
+  },
+];
