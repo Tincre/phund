bab83b405c61cc36f416c8f265425fc91eafbfbb
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-01-31 14:59:50 -0600

    :recycle: :white_check_mark: add mockSession object to index test [sc-546]

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,10 +2,44 @@
 
 import { render, screen } from "@testing-library/react";
 import Index from "../pages/index";
+import { SessionProvider } from "next-auth/react";
+
+export const mockSession = {
+  data: {
+    session: {
+      user: {
+        image: null,
+        name: "John",
+        email: "john@email.com",
+      },
+      expires: 123213139,
+      status: "loading",
+    },
+  },
+};
+describe("Index", () => {
+  it("should render the page", () => {
+    render(
+      <SessionProvider session={mockSession}>
+        <Index />
+      </SessionProvider>
+    );
+
+    const headingArray = screen.getAllByRole("heading");
+
+    headingArray.map((heading) => {
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
 
 describe("Index", () => {
   it("should render the page", () => {
-    render(<Index />);
+    render(
+      <SessionProvider session={undefined}>
+        <Index />
+      </SessionProvider>
+    );
 
     const headingArray = screen.getAllByRole("heading");
 
