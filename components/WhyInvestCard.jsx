8c00c57c0c7243f861198b44c0ae2c260ba0607b
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-01 21:45:35 -0600

    :recycle: add centered text w/padding on mobile [sc-645]

--- components/WhyInvestCard.jsx
+++ components/WhyInvestCard.jsx
@@ -1,11 +1,13 @@
 export default function WhyInvest1Card({ label, description }) {
   return (
-    <div className="w-full md:w-1/2 lg:w-full flex items-start flex-col lg:flex-row px-4 mb-8 lg:mb-6">
-      <span className="mr-10 mb-4 lg:mb-0 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-indigo-500 text-indigo-50 font-bold font-heading">
+    <div className="w-full md:w-1/2 lg:w-full flex items-center justify-center lg:justify-start flex-col lg:flex-row px-4 mb-8 lg:mb-6 ">
+      <span className="mb-4 lg:mb-0 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-indigo-500 text-indigo-50 font-bold font-heading">
         {label}
       </span>
       <div className="max-w-xs">
-        <p className="text-lg text-gray-500 leading-loose">{description}</p>
+        <p className="text-lg text-gray-500 leading-loose py-2 lg:pl-4 ">
+          {description}
+        </p>
       </div>
     </div>
   );
