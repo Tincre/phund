5005d5992ca36adb10de464674d2316d29b48836
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-01 22:08:37 -0600

    :recycle: move callback code to Newsletter component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,42 +1,18 @@
 import FooterColumn from "./FooterColumn";
 import Image from "next/image";
 import Logo from "./Logo";
-import { useState, useRef } from "react";
+import Newsletter from "./Newsletter";
 
-export default function Footer({ entityTitle, logoSrc, footerItems, socials }) {
-  const inputEl = useRef(null);
-  const [inputError, setInputError] = useState(false);
-  const [message, setMessage] = useState("");
-  const [subscribed, setSubscribed] = useState(false);
-
-  const subscribe = async (e) => {
-    e.preventDefault();
-
-    const res = await fetch(`/api/convertkit`, {
-      body: JSON.stringify({
-        email: inputEl.current.value,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    });
-
-    const { error } = await res.json();
-    if (error) {
-      setInputError(true);
-      setMessage(
-        "Your e-mail address is invalid or you are already subscribed!"
-      );
-      return;
-    }
-
-    inputEl.current.value = "";
-    setInputError(false);
-    setSubscribed(true);
-    setMessage("Successfully! 🎉 You are now subscribed.");
-  };
-
+export default function Footer({
+  entityTitle,
+  logoSrc,
+  footerItems,
+  socials,
+  newsletterTitle,
+  newsletterSubtitle,
+  newsletterName,
+  newsletterHref,
+}) {
   return (
     <section id="footer" className="py-20">
       <div className="container px-4 mx-auto">
@@ -46,52 +22,12 @@ export default function Footer({ entityTitle, logoSrc, footerItems, socials }) {
               <FooterColumn {...props} key={`${index}-footer-column`} />
             ))}
           </div>
-          <div className="order-first lg:order-last mb-10 lg:mb-0 w-full lg:w-2/5 lg:text-right">
-            <h3 className="mb-4 text-lg font-bold font-heading">Subscribe</h3>
-            <p className="mb-5 text-gray-500">
-              Stay informed with our updates via our blog,{" "}
-              <a
-                className="text-gray-700 hover:font-bold hover:underline hover:text-indigo-700"
-                href="https://slightlysharpe.com"
-              >
-                Slighly Sharpe
-              </a>
-              .
-            </p>
-            <form onSubmit={subscribe}>
-              <label htmlFor="email-input" className="sr-only">
-                Email address
-              </label>
-              <input
-                className="w-2/3 mb-4 pl-4 py-3 mr-4 border border-1 rounded border-indigo-200 hover:border-indigo-500 focus:border-indigo-800"
-                id="email-input"
-                name="email"
-                placeholder={
-                  subscribed ? "You're subscribed !  🎉" : "Enter your email"
-                }
-                ref={inputEl}
-                required
-                type="email"
-                disabled={subscribed}
-              />
-              <button
-                className={`w-1/4 inline-block px-5 py-3 text-sm bg-indigo-500 hover:bg-indigo-600 text-white font-semibold border border-indigo-500 hover:border-indigo-600 rounded transition duration-200 ${
-                  subscribed
-                    ? "cursor-default"
-                    : "hover:bg-indigo-700 dark:hover:bg-indigo-300"
-                } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600 dark:ring-offset-black`}
-                type="submit"
-                disabled={subscribed}
-              >
-                {subscribed ? "Thank you!" : "Sign up"}
-              </button>
-              {inputError && (
-                <div className="pt-2 text-sm text-red-500 w-72 sm:w-96 dark:text-red-400">
-                  {message}
-                </div>
-              )}
-            </form>
-          </div>
+          <Newsletter
+            newsletterHref={newsletterHref}
+            newsletterTitle={newsletterTitle}
+            newsletterSubtitle={newsletterSubtitle}
+            newsletterName={newsletterName}
+          />
         </div>
       </div>
       <div className="mt-4 lg:mt-16 mb-8 border-b border-indigo-50" />
@@ -111,7 +47,7 @@ export default function Footer({ entityTitle, logoSrc, footerItems, socials }) {
                 <a
                   className="flex leading-loose my-4 block text-sm text-gray-700 transform hover:scale-110 mr-4 lg:ml-4"
                   href={href}
-                  key={href}
+                  key={`${href}-${index}`}
                   rel="noopener noreferrer"
                 >
                   <div className="ml-auto md:m-auto text-center relative h-8 w-8">
