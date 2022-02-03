de502708e2136c52a86dcb685849a94e643f0dd6
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-03 13:27:59 -0600

    :recycle: polish content + add email notification for business [sc-755]

--- pages/api/email-me.js
+++ pages/api/email-me.js
@@ -3,11 +3,11 @@ import sgMail from "@sendgrid/mail";
 import fs from "fs";
 import {getSession} from "next-auth/react";
 import path from "path";
-import {resolveSafeTypeToFilename} from '../../lib/node-utils';
+import {resolveSafeTypeToFilename, sendEmailNotification} from '../../lib/node-utils';
 
 sgMail.setApiKey(process.env.EMAIL_API_KEY);
-
-
+const businessUserEmail = 'jason@tincre.com'
+const notificationMessage = 'The following user emailed themselves our SAFE note. You should probably follow up tiger. ;-)\n\n';
 export default async (req, res) => {
   if (req.method === "POST") {
     try {
@@ -22,7 +22,7 @@ export default async (req, res) => {
       const dir = path.resolve("./public", dirRelativeToPublicFolder);
       const pathToAttachment = `${dir}/${filename}`;
       const attachment = fs.readFileSync(pathToAttachment).toString("base64");
-      const message = "Please see your attached safe note.";
+      const message = "Hi!\n\nWe're thrilled that you're interested in investing to help us continue our growth. We will reach out shortly.\n\nMeanwhile, please find your requested SAFE note attached.";
       const msg = {
         to : session?.user?.email,
         from : process.env.FROM_EMAIL,
@@ -38,8 +38,14 @@ export default async (req, res) => {
         ],
       };
       await sgMail.send(msg);
-      console.log(`sent email to ${session?.user?.email}`);
-      res.status(200).json({message : `Email has been sent`});
+      const notificatWasSent = await sendEmailNotification(businessUserEmail, notificationMessage + `  - Investor email: ${session?.user?.email}`);
+      if (notificatWasSent) {
+        res.status(200).json({message: `Email has been sent and the business notified.`});
+       
+      }
+      else {
+        res.status(200).json({message: `Email has been sent`});
+      }
     } catch (error) {
       console.error(error);
       res.status(500).json({error : "Error sending email"});
