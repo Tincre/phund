aa84e2bfbaa9f77452316aa7c6c332b2d6f4756d
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-03 13:03:33 -0600

    :recycle: add <form> element wrapper on button for POST request [sc-755]

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -83,17 +83,19 @@ export default function Modal({
                 </div>
               </div>
               <div className="mt-5 sm:mt-6">
-                <button
-                  type="button"
-                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-                  onClick={(e) => {
-                    setOpen(false);
-                    setIsClicked(true);
-                    emailMe(e, safeType);
-                  }}
-                >
-                  {buttonText || "Email me this SAFE note"}
-                </button>
+                <form>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                    onClick={(e) => {
+                      setOpen(false);
+                      setIsClicked(true);
+                      emailMe(e, safeType);
+                    }}
+                  >
+                    {buttonText || "Email me this SAFE note"}
+                  </button>
+                </form>
               </div>
             </div>
           </Transition.Child>
