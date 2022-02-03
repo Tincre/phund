6e53050f70c7e4f328a697db659d03f0147bf865
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-02 21:45:48 -0600

    :fire: simplify to not render inline html or fancy tables

--- components/MarkdownWrapper.jsx
+++ components/MarkdownWrapper.jsx
@@ -1,16 +1,6 @@
 /* eslint-disable react/no-children-prop */
 import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import raw from "rehype-raw";
 
 export default function MarkdownWrapper({ id, content, className }) {
-  return (
-    <ReactMarkdown
-      id={id}
-      className={className}
-      children={content}
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[raw]}
-    />
-  );
+  return <ReactMarkdown id={id} className={className} children={content} />;
 }
