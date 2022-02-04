c6ebcec370e168fd054bff80847ee76f04e0ac68
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-04 02:37:23 -0600

    :art: :fire: remove px-4 directive

--- components/WhyInvestCard.jsx
+++ components/WhyInvestCard.jsx
@@ -1,6 +1,6 @@
 export default function WhyInvest1Card({ label, description }) {
   return (
-    <div className="w-full md:w-1/2 lg:w-full flex items-center justify-center lg:justify-start flex-col lg:flex-row px-4 mb-8 lg:mb-6 ">
+    <div className="w-full md:w-1/2 lg:w-full flex items-center justify-center lg:justify-start flex-col lg:flex-row mb-8 lg:mb-6 ">
       <span className="mb-4 lg:mb-0 flex-shrink-0 flex justify-center items-center w-12 h-12 rounded-full bg-indigo-500 text-indigo-50 font-bold font-heading">
         {label}
       </span>
