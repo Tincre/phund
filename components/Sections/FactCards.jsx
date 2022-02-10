5382c6f5a26a2c15538a6b0d8fbcf3cfffd72134
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-10 08:11:28 -0600

    :recycle: incorporate FactCard + FactSnippet changes [sc-793]

--- components/Sections/FactCards.jsx
+++ components/Sections/FactCards.jsx
@@ -8,18 +8,21 @@ export default function FactCards({ factCardsContent }) {
               <div
                 key={`${index}-fact-card`}
                 className="group w-full lg:w-1/2 p-4"
+                id={`${index}-fact-card`}
               >
-                <div className="py-12 px-10 rounded-lg bg-white group-hover:bg-indigo-800 transition hover:ease-in ease-out duration-300">
-                  <span className="mb-6 flex items-center justify-center w-12 h-12 rounded-full bg-indigo-500 group-hover:bg-indigo-200 text-indigo-50 group-hover:text-indigo-800 font-bold font-heading transition hover:ease-in ease-out duration-700">
-                    {index + 1}
-                  </span>
-                  <h3 className="mb-4 h-36 text-3xl leading-tight md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight font-bold font-heading group-hover:text-white transition hover:ease-in ease-out duration-300">
-                    {title}
-                  </h3>
-                  <p className="text-lg text-gray-500 leading-loose group-hover:text-indigo-100 transition hover:ease-in ease-out duration-700">
-                    {description}
-                  </p>
-                </div>
+                <a href={`#${index}-fact-snippet-card`}>
+                  <div className="py-12 px-10 md:h-96 rounded-lg bg-white group-hover:bg-indigo-800 transition hover:ease-in ease-out duration-300">
+                    <span className="mb-6 flex items-center justify-center w-12 h-12 rounded-full bg-indigo-500 group-hover:bg-indigo-200 text-indigo-50 group-hover:text-indigo-800 font-bold font-heading transition hover:ease-in ease-out duration-700">
+                      {index + 1}
+                    </span>
+                    <h3 className="mb-4 md:h-36 text-3xl leading-tight md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight font-bold font-heading group-hover:text-white transition hover:ease-in ease-out duration-300">
+                      {title}
+                    </h3>
+                    <p className="text-lg text-gray-500 leading-loose group-hover:text-indigo-100 transition hover:ease-in ease-out duration-700">
+                      {description}
+                    </p>
+                  </div>
+                </a>
               </div>
             );
           })}
--- components/Sections/FactSnippets.jsx
+++ components/Sections/FactSnippets.jsx
@@ -28,21 +28,30 @@ export default function FactSnippets({
             ({ description, imageSrc, title, subTitle }, index) => {
               return (
                 <div
-                  className="w-full md:w-1/2 lg:w-1/3 p-4"
+                  className="group w-full md:w-1/2 lg:w-1/3 p-4"
                   key={`${index}-fact-snippet-card`}
                 >
-                  <div className="bg-gray-50 p-6 rounded-lg">
-                    <p className="mb-8 text-base leading-relaxed lg:text-xl lg:leading-relaxed text-gray-500">
-                      {description}
-                    </p>
-                    <img
-                      className="mb-2 h-12 w-12 object-cover rounded-full mx-auto"
-                      src={imageSrc}
-                      alt={`${title} fact snippet heading object`}
-                    />
-                    <p className="text-2xl font-bold font-heading">{title}</p>
-                    <p className="text-lg text-gray-400">{subTitle}</p>
-                  </div>
+                  <a href={`#${index}-fact-card`}>
+                    <div
+                      id={`${index}-fact-snippet-card`}
+                      className="bg-gray-50 group-hover:bg-indigo-800 p-6 md:h-72 rounded-lg group-hover:ease-in ease-out duration-300"
+                    >
+                      <p className="mb-8 text-base leading-relaxed lg:text-xl lg:leading-relaxed text-gray-500 group-hover:text-indigo-50 transition group-hover:ease-in ease-out duration-700">
+                        {description}
+                      </p>
+                      <img
+                        className="mb-2 h-12 w-12 object-cover rounded-full mx-auto group-hover:ease-in ease-out duration-300"
+                        src={imageSrc}
+                        alt={`${title} fact snippet heading object`}
+                      />
+                      <p className="text-2xl font-bold font-heading group-hover:text-white group-hover:ease-in ease-out duration-300">
+                        {title}
+                      </p>
+                      <p className="text-lg text-gray-400 group-hover:text-indigo-200 group-hover:ease-in ease-out duration-500">
+                        {subTitle}
+                      </p>
+                    </div>
+                  </a>
                 </div>
               );
             }
