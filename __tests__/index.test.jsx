b95a15284fe8c6b70d710ce95807b94707618806
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-24 17:47:30 -0600

    :recycle: add additional next/head mock + coverage

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,8 +1,18 @@
 // __tests__/index.test.jsx
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Index from "../pages/index";
 import { SessionProvider } from "next-auth/react";
+
+jest.mock("next/head", () => {
+  return {
+    __esModule: true,
+    default: ({ children }) => {
+      return <>{children}</>;
+    },
+  };
+});
+
 global.fetch = jest.fn(() =>
   Promise.resolve({
     json: () => Promise.resolve({}),
@@ -12,20 +22,21 @@ global.fetch = jest.fn(() =>
 beforeEach(() => {
   fetch.mockClear();
 });
-export const mockSession = {
-  data: {
-    session: {
-      user: {
-        image: null,
-        name: "John",
-        email: "john@email.com",
+
+describe("Index", () => {
+  const mockSession = {
+    data: {
+      session: {
+        user: {
+          image: null,
+          name: "John",
+          email: "john@email.com",
+        },
       },
     },
-  },
-  expires: 123213139,
-  status: "loading",
-};
-describe("Index", () => {
+    expires: 123213139,
+    status: "loading",
+  };
   it("should render the page", () => {
     render(
       <SessionProvider session={mockSession}>
@@ -39,9 +50,35 @@ describe("Index", () => {
       expect(heading).toBeInTheDocument();
     });
   });
+  it("should fire the event", () => {
+    render(
+      <SessionProvider session={mockSession}>
+        <Index />
+      </SessionProvider>
+    );
+
+    const button = screen.getByRole("button", { name: /Mobile Menu/i });
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    //expect(button).toBeInTheDocument();
+  });
 });
 
 describe("Index", () => {
+  const mockSession = {
+    data: {
+      session: {
+        user: {
+          image: null,
+          name: "John",
+          email: "john@email.com",
+        },
+      },
+    },
+    expires: 123213139,
+    status: "authenticated",
+  };
+
   it("should render the page", () => {
     render(
       <SessionProvider session={undefined}>
