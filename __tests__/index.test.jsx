39341ca7964f4c5eb0e6d77e181ef9eec9c7bc78
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-24 17:12:50 -0600

    :recycle: :white_check_mark: :wrench: add fetch mock

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -3,7 +3,15 @@
 import { render, screen } from "@testing-library/react";
 import Index from "../pages/index";
 import { SessionProvider } from "next-auth/react";
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  })
+);
 
+beforeEach(() => {
+  fetch.mockClear();
+});
 export const mockSession = {
   data: {
     session: {
@@ -12,10 +20,10 @@ export const mockSession = {
         name: "John",
         email: "john@email.com",
       },
-      expires: 123213139,
-      status: "loading",
     },
   },
+  expires: 123213139,
+  status: "loading",
 };
 describe("Index", () => {
   it("should render the page", () => {
